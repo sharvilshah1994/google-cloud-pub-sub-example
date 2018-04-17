76e7d2c8a6ec36bc7efa0af851fa9d323d7023ed
sharvil <sshah-consultant@scholastic.com>
2018-04-17 00:42:50 -0700

    initial commit with gcp & jpa setup

---
+++ src/main/java/com/cloudproject/apptier/SavemeApptierApplication.java
@@ -0,0 +1,14 @@
+package com.cloudproject.apptier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class SavemeApptierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavemeApptierApplication.class, args);
+	}
+}
